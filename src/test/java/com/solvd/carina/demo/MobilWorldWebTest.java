9a54152c8d83b6f6eab39d04ffbf2f0805f00e6c
akhivyk <akhivyk@solvd.com>
2023-06-12 17:44:35 +0300

    added test to open website with cookie

--- src/test/java/com/solvd/carina/demo/MobilWorldWebTest.java
+++ src/test/java/com/solvd/carina/demo/MobilWorldWebTest.java
@@ -3,17 +3,24 @@ package com.solvd.carina.demo;
 import com.solvd.carina.demo.gui.mobilworld.components.PopUpLoginWindow;
 import com.solvd.carina.demo.gui.mobilworld.components.TopMenu;
 import com.solvd.carina.demo.gui.mobilworld.desktop.*;
+import com.zebrunner.agent.core.annotation.TestRailCaseId;
 import com.zebrunner.carina.core.AbstractTest;
 import com.zebrunner.carina.utils.R;
+import jdk.jfr.Name;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Set;
 
 public class MobilWorldWebTest extends AbstractTest {
     private static final Logger LOGGER = LogManager.getLogger();
 
     @Test
+    @TestRailCaseId("C3559")
     public void testLogin() {
         MainPage mainPage = new MainPage(getDriver());
         mainPage.open();
@@ -33,6 +40,7 @@ public class MobilWorldWebTest extends AbstractTest {
     }
 
     @Test
+    @TestRailCaseId("C3560")
     public void addToCartIphone() {
         String model = "iPhone 14 Pro Max";
         String model2 = "iPhone 14 PRO Max 256 GB - Темно-фиолетовый";
@@ -56,6 +64,7 @@ public class MobilWorldWebTest extends AbstractTest {
     }
 
     @Test
+    @TestRailCaseId("C3561")
     public void testDeleteItemsFromCart() {
         String model = "iPhone 14 Pro Max";
         String model2 = "iPhone 14 PRO Max 256 GB - Темно-фиолетовый";
@@ -82,6 +91,7 @@ public class MobilWorldWebTest extends AbstractTest {
     }
 
     @Test
+    @TestRailCaseId("C3562")
     public void testAllPagesAvailable() {
         MainPage mainPage = new MainPage(getDriver());
         mainPage.open();
@@ -104,6 +114,7 @@ public class MobilWorldWebTest extends AbstractTest {
     }
 
     @Test
+    @TestRailCaseId("C3563")
     public void testProductInfo() {
         String model = "iPhone 14 Pro Max";
         String model2 = "iPhone 14 PRO Max 256 GB - Темно-фиолетовый";
@@ -139,5 +150,27 @@ public class MobilWorldWebTest extends AbstractTest {
         Assert.assertFalse(iphonePage.getModels().isEmpty(), "Models is not found");
 
         iphonePage.getModels().forEach(a -> LOGGER.info(a.getText()));
+    }
+
+    @Test(testName = "MobilWorld - testOpenWebSiteWithCookie")
+    public void testOpenWebSiteWithCookie() {
+        String cookieName = "session-username";
+        String cookieValue = "standard_user";
+        WebDriver driver = getDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        Cookie cookie = new Cookie(cookieName, cookieValue);
+        driver.manage().addCookie(cookie);
+        driver.get("https://www.saucedemo.com/inventory.html");
+
+        Set<Cookie> cookies = driver.manage().getCookies();
+        boolean cookieFound = false;
+        for (Cookie cookie1 : cookies) {
+            if (cookie1.getName().equals(cookieName) && cookie1.getValue().equals(cookieValue)) {
+                cookieFound = true;
+                break;
+            }
+        }
+        Assert.assertTrue(cookieFound, "Cookie " + cookieName + " with value " + cookieValue + " not found");
     }
 }
